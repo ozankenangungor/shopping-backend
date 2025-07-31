cdae9a1db0855275fd2e0da41b2714bb0c9cad13
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-07-31 23:05:27 +0300

    Product's Image Upload Serve Static

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,22 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateProductRequest } from './dto/create-product.request';
 import { CurrentUser } from '../auth/current-user.decorator';
 import { TokenPayload } from '../auth/token-payload.interface';
 import { ProductsService } from './products.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
 @Controller('products')
@@ -16,5 +30,39 @@ export class ProductsController {
     @CurrentUser() user: TokenPayload,
   ) {
     return this.productsService.createProduct(body, user.userId);
+  }
+
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        filename: (req, file, callback) => {
+          callback(
+            null,
+            `${req.params.productId}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+    )
+    
+  uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    _file: Express.Multer.File,
+  ) {}
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getProducts() {
+    return this.productsService.getProducts();
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,8 @@
+import { promises as fs } from 'fs';
 import { Injectable } from '@nestjs/common';
 import { CreateProductRequest } from './dto/create-product.request';
 import { PrismaService } from '../prisma/prisma.service';
+import { join } from 'path';
 
 @Injectable()
 export class ProductsService {
@@ -13,5 +15,27 @@ export class ProductsService {
         userId,
       },
     });
+  }
+
+  async getProducts() {
+    const products = await this.prismaService.product.findMany();
+    return Promise.all(
+      products.map(async (product) => ({
+        ...product,
+        imageExists: await this.imageExists(product.id),
+      })),
+    );
+  }
+
+  private async imageExists(productId: number) {
+    try {
+      await fs.access(
+        join(__dirname, '../../', `public/products/${productId}.jpg`),
+        fs.constants.F_OK,
+      );
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
